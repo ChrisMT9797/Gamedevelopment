06dc603609874db7ce02089d3b3713b30bdab2a3
ChrisMT9797 <10723739@uvu.edu>
2022-02-14 14:10:03 -0700

    Added VarNOpps script
    Withing script I added a few int, double, char, and string. The script isn't working. Seems that it doesn't want unused variables for one, for two I need to make sure if the opp goes in void update or void start. I have them in void update, so I may try them in void start. I also need to review the Console.WriteLine... etc., to make sure I have it correct, or to see how it changes between types. I also wanted to put a string with a char (character first name last initial sort of thing), so I'll need to learn to do that. Might be something line "ToString".

---
+++ Example Game (1)/Assets/New Unity Project/Assets/VarNOpp.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VarNOpp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int n1st = 1;
+		int n2nd = 2;
+		int n3rd = 3;
+		int n4th = 4;
+		int n5th = 5;
+		double d1st = 1.45;
+		double d2nd = 4.66;
+		double d3rd = 7.67;
+		char c1st = A;
+		char c2nd = C;
+		char c3rd = E;
+		string name1st = Ral;
+		string name2nd = Gabby;
+		string name3rd = Tristian;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Console.WrightLine(n1st + n5th);
+		/* Console.WrightLine(name1st +c3rd);
+*/
+    }
+}
