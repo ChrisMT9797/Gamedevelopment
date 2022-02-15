3a211088357f77bff7793dd547573a1e31beb536
ChrisMT9797 <10723739@uvu.edu>
2022-02-15 16:04:32 -0700

    Started fixing varNOpps
    getting an error "'Console' does not exist in the current context." so messaging teach for help.

--- Example Game (1)/Assets/New Unity Project/Assets/VarNOpp.cs
+++ Example Game (1)/Assets/New Unity Project/Assets/VarNOpp.cs
@@ -7,27 +7,31 @@ public class VarNOpp : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int n1st = 1;
-		int n2nd = 2;
-		int n3rd = 3;
-		int n4th = 4;
-		int n5th = 5;
-		double d1st = 1.45;
-		double d2nd = 4.66;
-		double d3rd = 7.67;
-		char c1st = A;
-		char c2nd = C;
-		char c3rd = E;
-		string name1st = Ral;
-		string name2nd = Gabby;
-		string name3rd = Tristian;
+        int num1 = 1;
+		int num2 = 2;
+		int num3 = 3;
+		int num4 = 4;
+		int num5 = 5;
+		//double d1st = 1.45;
+		//double d2nd = 4.66;
+		//double d3rd = 7.67;
+		//char c1st = A;
+		//char c2nd = C;
+		//char c3rd = E;
+		//string name1st = Ral;
+		//string name2nd = Gabby;
+		//string name3rd = Tristian;
+		int sum1 = num1 + num2;
+		int sum2 = num2 + num3 + num4;
+		int sum3 = num1 + num4 + num5;
+		Console.WriteLine(sum1); // Print the sum of x+y
+		Console.WriteLine(sum2);
+		Console.WriteLine(Sum3);
     }
 
     // Update is called once per frame
     void Update()
     {
-		Console.WrightLine(n1st + n5th);
-		/* Console.WrightLine(name1st +c3rd);
-*/
+
     }
 }
